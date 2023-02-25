355001918070ece3971e339783ee07ee32ec3708
Parmedio <marco.da.pieve@gmail.com>
2023-02-26 00:15:36 +0100

    filtri...

--- src/containers/App.js
+++ src/containers/App.js
@@ -55,6 +55,7 @@ function App() {
   };
 
   const clearFilters = () => {
+    setPokeIndex(prevPokeIndex)
     setFilters([]);
   };
 
@@ -77,7 +78,9 @@ function App() {
   };
 
   const getFilter = (event) => {
+    setWay('forward')
     const filter = event.target.getAttribute('filter');
+    setPokeIndex(prevPokeIndex)
     setFilters(prevFilters => {
       if (prevFilters.includes(filter)) {
         return prevFilters.filter(f => f !== filter);
@@ -88,61 +91,32 @@ function App() {
     });
   };
 
-  const pkmonObjBuilder = async (number) =>{
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
-    const obj = await res.json();
-    const Order = obj.id;
-    const Name = obj.name;
-    const Type01 = obj.types[0].type.name;
-    let Type02 = '' 
-    try {
-    Type02 = obj.types[1].type.name;
-    }
-    catch {
-    Type02 = '';
-    }
-    const Height = obj.height;
-    const Weight = obj.weight;
-    const OffArt = obj.sprites.other['official-artwork'].front_default;
-    const Sprite = obj.sprites.front_default;
-  
-    const newPkmon = {
-      number: Order,
-      name: Name,
-      type01: Type01,
-      type02: Type02,
-      height: Height,
-      weight: Weight,
-      offArt: OffArt,
-      sprite: Sprite
-    }
-    return newPkmon
-  }
-
-  const adjustIndexj = (span, direction, prevDirection) => { // CI SARà PREVINDEX
-    if (prevDirection !== null && prevDirection !== direction) {
-      console.log('-----------------------------------------------------> hai cambiato senso di direzione');
-      if (prevDirection === 'backward') {
-        console.log('>>> ho impostato come indice ' + (pokeIndex + (span + 1)));
-        return (pokeIndex + (span + 1))
-      } else if (prevDirection === 'forward') {
-        console.log('>>> ho impostato come indice ' + (pokeIndex - (span + 1)));
-        return (pokeIndex - (span + 1))        
-      }
-    } else {
-      return pokeIndex
-    }
+const pkmonObjBuilder = async (number) => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+  const obj = await res.json();
+  const newPkmon = {
+    number: obj.id,
+    name: obj.name,
+    type01: obj.types[0].type.name,
+    type02: obj.types[1] ? obj.types[1].type.name : '',
+    height: obj.height,
+    weight: obj.weight,
+    offArt: obj.sprites.other['official-artwork'].front_default,
+    sprite: obj.sprites.front_default
   };
+  return newPkmon;
+};
 
-  const adjustIndex = (span, direction, prevDirection) => { // CI SARà PREVINDEX
+
+  const adjustIndex = (direction, prevDirection) => {
     if (prevDirection !== null && prevDirection !== direction) {
       console.log('-----------------------------------------------------> hai cambiato senso di direzione');
       if (prevDirection === 'backward') {
-        console.log('>>> ho impostato come indice ' + (pokeIndex + (span + 1)));
-        return (prevPokeIndex + 1)
+        console.log('>>> ho impostato come indice ' + (prevPokeIndex + 1));
+        return (prevPokeIndex +1)
       } else if (prevDirection === 'forward') {
-        console.log('>>> ho impostato come indice ' + (pokeIndex - (span + 1)));
-        return (prevPokeIndex - 1)
+        console.log('>>> ho impostato come indice ' + (prevPokeIndex));
+        return (prevPokeIndex -1)
       }
     } else {
       return pokeIndex
@@ -153,12 +127,9 @@ function App() {
     let carico = 0;
     let newBasket = [];
     let basketDepth = perPage
-    let currentIndex = adjustIndex(basketDepth, direction, direction !== way ? way : null)
-    setPrevPokeIndex(currentIndex)
-
-
-
-    console.log('inizio con indice        : ' + currentIndex)
+    let currentIndex = adjustIndex(direction, direction !== way ? way : null)
+    //setPrevPokeIndex(currentIndex)
+    console.log('inizio con indice : ' + currentIndex)
     console.log('loadPkmon sto facendo way      : ' + direction)
     while (newBasket.length < basketDepth ) {
       let pokemonNr = pokeNumberAtIndex(currentIndex)
@@ -181,22 +152,25 @@ function App() {
         }
       }
     }
-    setPokeIndex(currentIndex);
-    console.log('finisco con indice       : ' + currentIndex)
-    console.log('======================================== impostato prevPokeIndex   : ' + prevPokeIndex)
-    console.log('======================================== impostato pokeIndex       : ' + pokeIndex)
-    setTimeout(() => {setLoad(0.5)}, 400)
+    //setPokeIndex(currentIndex);
+    console.log('finisco con indice: ' + currentIndex)
+    console.log('impostato prevPokeIndex   : ' + pokeIndex)
+    console.log('impostato pokeIndex       : ' + currentIndex)
     console.log('====================================')
+    setTimeout(() => {setLoad(0.5)}, 400)
     if (direction === "backward") {
+      setPrevPokeIndex((newBasket[0].number));////
+      setPokeIndex((newBasket[perPage-1].number));////
       return reverseArray(newBasket)
     } else{
+      setPrevPokeIndex((newBasket[0].number));////
+      setPokeIndex((newBasket[perPage-1].number));////
       return newBasket
     }
   };
 
   const updateCardList = async (event) => {
     const currentWay = getWay(event);
-    //const adjustedIndex = pokeIndex;
     let list = await loadPkmon(currentWay);
     setDisplayedPkmon(list);
   };
